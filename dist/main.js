40a87ced6140a95c00fdea1bb8ccf412fbc49e7e
Damian <dsimrayh@yahoo.com>
2022-07-20 21:30:35 -0700

    HTML skeleton built, basic styling, header/ footer added

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("webpack");
+(()=>{const e=document.querySelector("a"),o=document.querySelector("#gh-link"),t=document.querySelector("#gh-logo");e.addEventListener("mouseover",(()=>{o.classList.toggle("link-hover"),t.classList.toggle("logo-hover")})),e.addEventListener("mouseout",(()=>{o.classList.toggle("link-hover"),t.classList.toggle("logo-hover")}))})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("webpack");
+// GitHub Link - style changes on hover
+const githubLink = document.querySelector('a');
+const link = document.querySelector('#gh-link');
+const logo = document.querySelector('#gh-logo');
+
+githubLink.addEventListener('mouseover', () => {
+    link.classList.toggle('link-hover');
+    logo.classList.toggle('logo-hover');
+})
+
+githubLink.addEventListener('mouseout', () => {
+    link.classList.toggle('link-hover');
+    logo.classList.toggle('logo-hover');
+})
