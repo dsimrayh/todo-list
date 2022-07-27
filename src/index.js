5f45e96ad925cacfa4f8dac0eceedd903ba81871
Damian <dsimrayh@yahoo.com>
2022-07-26 22:34:09 -0700

    Basic styling updated, navbar contents added  and style updated

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ const logo = document.querySelector('#gh-logo');
 githubLink.addEventListener('mouseover', () => {
     link.classList.toggle('link-hover');
     logo.classList.toggle('logo-hover');
-})
+});
 
 githubLink.addEventListener('mouseout', () => {
     link.classList.toggle('link-hover');
     logo.classList.toggle('logo-hover');
-})
+});
+
+
+// Menu Button
+const menuButton = document.querySelector('#menu-button');
+
+menuButton.addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('navbar-hidden');
+    console.log('click');
+});
